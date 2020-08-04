55a63f3e02ac68311a1fd488d1572d5a64a2fe7f
mattcosta617 <mattiecosta@gmail.com>
2020-08-04 18:24:11 -0400

    models setup

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=150)
+    picture = models.FileField(upload_to='uploads/')
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    picture = models.FileField(upload_to='uploads/')
+    date = models.DateField()
+    
