1cd314d92d1df3d2242d8045ada0c4af7e08e356
mattcosta617 <mattiecosta@gmail.com>
2020-08-04 19:29:22 -0400

    users model added, initial migration, picture upload?

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class City(models.Model):
@@ -7,9 +8,19 @@ class City(models.Model):
     country = models.CharField(max_length=150)
     picture = models.FileField(upload_to='uploads/')
 
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     picture = models.FileField(upload_to='uploads/')
     date = models.DateField()
-    
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=150)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
