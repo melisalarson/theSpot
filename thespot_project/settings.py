3c3fbee6960c45e7513c978da4b0788fc76d61ea
melisalarson <larsonmelisa@gmail.com>
2020-08-10 09:36:40 -0700

    heroku deployment

--- thespot_project/settings.py
+++ thespot_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
 ]
+
+django_on_heroku.settings(locals())
